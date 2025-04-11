b3eb2f36b848e7c384984dd324850c3995a90123
insu <hinsu93@gmail.com>
2025-04-11 18:52:40 +0900

    GraphQL 통합설정

---
+++ apps/api/src/comment/comment.resolver.ts
@@ -0,0 +1,9 @@
+import { Resolver } from '@nestjs/graphql';
+
+import { CommentService } from './comment.service';
+import { CommentEntity } from './entities/comment.entity';
+
+@Resolver(() => CommentEntity)
+export class CommentResolver {
+  constructor(private readonly commentService: CommentService) {}
+}
